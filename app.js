9b27f37beb3debf523838caffbcdf13e3685c2ce
Joseph Musenge <josephmusengep62@gmail.com>
2023-05-20 15:34:58 -0500

    final project

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // Variables 
 const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
+const selected = document.querySelector(".selected");
 
 // Listen for click event on modal button
-modalBtn.addEventListener("click", function(){
+modalBtn.addEventListener("click", function (){
     modal.classList.add("open-modal");
-})
+});
+
+// get number from clicked number
+const numbers = document.querySelectorAll(".number");
+numbers.forEach(function (btn){
+    btn.addEventListener("click", function(e){
+        const value = e.currentTarget.textContent;
+        selected.textContent = `You selected ${value} out of 5`;
+    });
+});
+
+
